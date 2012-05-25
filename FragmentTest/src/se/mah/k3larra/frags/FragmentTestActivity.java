4f747809b794e3ed320169e2f5c6925e9b1acbd1
Lars <k3larra@hotmail.com>
2012-05-25 11:52:03 +0200

    hjhjh

--- FragmentTest/src/se/mah/k3larra/frags/FragmentTestActivity.java
+++ FragmentTest/src/se/mah/k3larra/frags/FragmentTestActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class FragmentTestActivity extends Activity {
     /** Called when the activity is first created. */
+	//sdsdsdsd
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
